c82b7dbe718c2e44a3bafe9033d13ad7cb5117d9
grahn <grahn>
2011-01-02 21:44:04 +0000

    class JpegDim implemented

--- anydim.cc
+++ anydim.cc
@@ -1,4 +1,4 @@
-/* $Id: anydim.cc,v 1.8 2010-12-31 09:07:54 grahn Exp $
+/* $Id: anydim.cc,v 1.9 2011-01-02 21:44:04 grahn Exp $
  *
  * Copyright (c) 2010 Jörgen Grahn
  * All rights reserved.
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 #include <cstdlib>
 #include <cassert>
@@ -129,6 +130,14 @@ namespace {
 	return n;
     }
 
+    unsigned eat16(const uint8_t*& p)
+    {
+	unsigned n = *p++ << 8;
+	n <<= 8;
+	n |= *p++;
+	return n;
+    }
+
     void ignore(std::istream& is, unsigned n)
     {
 	is.ignore(n);
@@ -148,14 +157,142 @@ namespace {
     class JpegDim {
     public:
 	JpegDim();
-	void feed(const uint8_t *buf, size_t count);
+	void feed(const uint8_t *a, const uint8_t *b);
 	void eof();
 
 	bool bad() const;
 	bool measured() const;
 
+	unsigned width;
+	unsigned height;
+
     private:
+	std::vector<uint8_t> mem_;
+
+	enum Marker {
+	    SOI  = 0xffd8,
+	    SOF0 = 0xffc0,
+	    SOF2 = 0xffc2,
+	    DHT  = 0xffc4,
+	    DQT  = 0xffdb,
+	    DRI  = 0xffdd,
+	    SOS  = 0xffda,
+	    APP0 = 0xffe0,
+	    APP1 = 0xffe1,
+	    COM  = 0xfffe,
+	    EOI  = 0xffd9
+	};
+
+	enum State { UNDECIDED, GOOD, BAD };
+	State state_;
+	bool in_entropy_;
+	unsigned seen_;
+
+	void eat_entropy(const uint8_t *&a, const uint8_t *b);
     };
+
+
+    JpegDim::JpegDim()
+	: state_(UNDECIDED),
+	  in_entropy_(false),
+	  seen_(0)
+    {}
+
+    void JpegDim::feed(const uint8_t *a, const uint8_t *b)
+    {
+	if(state_==BAD) return;
+
+	std::vector<uint8_t> ws;
+
+	if(!mem_.empty()) {
+	    std::swap(ws, mem_);
+	    ws.insert(ws.end(), a, b);
+	    a = &ws[0];
+	    b = a + ws.size();
+	}
+
+	if(in_entropy_)	eat_entropy(a, b);
+
+	while(!in_entropy_) {
+
+	    if(b-a<2) break;
+	    const unsigned m = eat16(a);
+	    if(seen_==0 && m!=SOI) {
+		state_ = BAD;
+		break;
+	    }
+	    if(m>>8 != 0xff) {
+		state_ = BAD;
+		break;
+	    }
+
+	    if(0xffd0 <= m && m <= 0xffd9) {
+		/* just a marker */
+		continue;
+	    }
+
+	    /* marker, length, data */
+	    if(b-a<2) {
+		a-=2;
+		break;
+	    }
+	    unsigned n = eat16(a);
+	    if(n<2) break;
+	    n-=2;
+	    if(b-a<n) {
+		a-=2+2;
+		break;
+	    }
+
+	    if(m==SOF0 || m==SOF2) {
+		if(n<5) {
+		    state_ = BAD;
+		    break;
+		}
+		state_ = GOOD;
+		a++;
+		height = eat16(a);
+		width = eat16(a);
+		a += n<1+2+2;
+	    }
+	    else {
+		a += n;
+	    }
+
+	    eat_entropy(a, b);
+	}
+
+	if(a!=b) {
+	    mem_.insert(mem_.end(), a, b);
+	}
+    }
+
+
+    /**
+     * Consume entropy-encoded data from [a, b) until all is consumed
+     * or a segment is found. Sets 'in_entropy_' and updates 'a' and 'seen_'.
+     */
+    void JpegDim::eat_entropy(const uint8_t *&a, const uint8_t *b)
+    {
+	const uint8_t* const c = a;
+
+	in_entropy_ = true;
+	while(a!=b) {
+	    if(*a++==0xff) {
+		if(a==b) {
+		    a--;
+		    break;
+		}
+		else if(*a) {
+		    a--;
+		    in_entropy_ = false;
+		    break;
+		}
+	    }
+	}
+
+	seen_ += a-c;
+    }
 
 
     void jpegdim(std::istream& is,
