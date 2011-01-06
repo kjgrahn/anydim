4671eae5247178062dc1b97a77bde99efb80c7d6
grahn <grahn>
2011-01-06 09:38:16 +0000

    *** empty log message ***

---
+++ test/dim.cc
@@ -0,0 +1,14 @@
+/**
+ * $Id: dim.cc,v 1.1 2011-01-06 09:38:16 grahn Exp $
+ *
+ * Copyright (c) 2011 Jörgen Grahn
+ * All rights reserved.
+ *
+ * This code assumes certain image files exist and are 48 × 21 pixels.
+ * test/anydim.ppm
+ * 
+ */
+#include <anydim.h>
+#include <testicle.h>
+
+
